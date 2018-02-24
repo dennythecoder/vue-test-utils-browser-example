35558cc4c49595522dfda72fffb7d5948b0d6303
Denny <dennythecoder@gmail.com>
2018-02-23 22:09:47 -0600

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # vue-test-utils-browser-example
+
+The purpose of this repo is to show how to do Vue component testing without modules/node tools.  The [example](./example.html) should display some tests.
---
+++ example.js
@@ -0,0 +1,19 @@
+var components = (function(Vue){
+
+  var superCounter  = Vue.component('super-counter',{
+    data:function(){
+      return {
+        count:0
+      }
+    },
+    template:'<div @click="count++">{{count}}</div>'
+  });
+
+  return {
+    superCounter:superCounter
+  };
+
+})(Vue);
+
+
+
